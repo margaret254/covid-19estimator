a5430a4c584fdff7917e4930b35753b1f9d0346f
Margaret mwangi <maggiemwas91@gmail.com>
2020-04-11 22:45:31 +0300

    Add to functions estimator and data in the estimator.py file

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,30 @@
+
 def estimator(data):
-  return data
+      currentlyInfectedImpact = int(data['reportedCases'] * 10)
+      currentlyInfectedSevereImpact = int(data['reportedCases'] * 50)
+      days = timeEstimate(data)
+
+      return{
+        "data":data,
+        "impact":{
+          "currentlyInfected": currentlyInfectedImpact,
+          "infectionsByRequestedTime": currentlyInfectedImpact * (2 ** (int(days / 3)))
+        },
+        "severeImpact":{
+          "currentlyInfected": currentlyInfectedImpact,
+          "infectionsByRequestedTime": currentlyInfectedSevereImpact * (2 ** (int(days / 3)))
+        }
+        
+      }
+
+
+def timeEstimate(data):
+      if data['periodType'] == 'days':
+              days = data['timeToElapse']
+
+      elif data['periodType'] == 'weeks':
+              days = data['timeToElapse'] * 7
+
+      elif data['periodType'] == 'months':
+              days = data['timeToElapse'] * 30
+      return days
